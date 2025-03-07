19a9441c7a53f787f963ffd6a7790fd9aa8ea1ca
JunyaoLin-ucsc <jlin232@ucsc.edu>
2025-03-07 05:05:37 -0800

    added a bunch of stuff

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 let config = {
     type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
     width: 1280,
     height: 720,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
     scene: [ MainMenu, Tutorial, Gameplay, Gameover ]
 };
 
-let game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
