aff92dd7d2e7884e976475b97c1b53425adc291c
JunyaoLin-ucsc <jlin232@ucsc.edu>
2025-03-14 02:36:52 -0700

    ceshi

--- src/scenes/Gameplay.js
+++ src/scenes/Gameplay.js
@@ -153,7 +153,7 @@ class Gameplay extends Phaser.Scene {
     this.createRalph();
 
     // 调用放大函数，将 Ralph 放大（例如放大到 0.5 倍）
-    this.enlargeRalph(1.5);
+    this.enlargeRalph(1);
 
     this.loadStageObjectLayers(1);
 
