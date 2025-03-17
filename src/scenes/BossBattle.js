6ce8c880bf3548195dd2f1493edcfdb2e1f5d7c5
JunyaoLin-ucsc <jlin232@ucsc.edu>
2025-03-17 02:29:14 -0700

    offser laser hitbox

--- src/scenes/BossBattle.js
+++ src/scenes/BossBattle.js
@@ -606,8 +606,8 @@ class BossBattle extends Phaser.Scene {
       laserY = this.angryRalph.y - 70;
       bodyW = 2560; 
       bodyH = 200;
-      offsetX = -120; 
-      offsetY = 50; 
+      offsetX = -200; 
+      offsetY = -100; 
     } else {
       // ===========【手持激光炮】===========
       if (direction === "right") {
